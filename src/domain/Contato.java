846fce77d5973466fd97fa174dbdd61fdaa7d2a1
Engineer Alejandro Malis <eng.alejandromalisdiaz@gmail.com>
2026-07-20 20:57:29 -0300

    Criação da camada domain com o arquivo que representa a entidade Contato

--- src/domain/Contato.java
+++ src/domain/Contato.java
@@ -1,3 +1,53 @@
 Public class Contato {
 
+    private String id;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    // Constructor
+    public Contato(
+            String id,
+            String nome,
+            String telefone,
+            String email
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
